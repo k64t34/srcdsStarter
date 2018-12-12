f83f8feaad8695e6187ce091163e2c81799adfe6
k64t34 <k64t34@gmail.com>
2018-12-12 18:17:02 +0300

    0

---
+++ srcdsStarter/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: skorik
+ * Date: 12.12.2018
+ * Time: 18:13
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace srcdsStarter
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
